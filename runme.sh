2893f821ccb8df0473bd6315551ede61f81c43af
cmg <cmg@cmg-virtualbox.hsd1.ga.comcast.net>
2011-11-10 10:39:54 -0500

    so I don't forget how I like to set this up

---
+++ runme.sh
@@ -0,0 +1,9 @@
+cd ~
+ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.vim/gvimrc ~/.gvimrc
+cd ~/.vim
+git submodule init
+git submodule update
+cd ~/.vim/bundle/pyflakes/ftplugin/python
+git clone http://github.com/kevinw/pyflakes  
+cd ~
