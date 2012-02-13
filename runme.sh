6d95a0481cf21fde760642482ceb527b3c19dad6
= <=>
2012-02-12 20:19:45 -0500

    this work the way I want it to now

--- runme.sh
+++ runme.sh
@@ -1,10 +1,10 @@
-pushd
 cd ~
-ln -s ~/.vim/vimrc ~/.vimrc
-ln -s ~/.vim/gvimrc ~/.gvimrc
-cd ~/.vim
+ln -fs ~/dotfiles/.vim ~/.vim
+ln -fs ~/dotfiles/.vimrc ~/.vimrc
+cd ~/dotfiles
 git submodule init
 git submodule update
-cd ~/.vim/bundle/pyflakes/ftplugin/python
-git clone http://github.com/kevinw/pyflakes  
-popd
+cd ~/dotfiles/.vim/bundle/pyflakes-vim
+git submodule init
+git submodule update
+cd ~
